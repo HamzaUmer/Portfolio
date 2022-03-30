8c5f6cac9fc4832d9d22c8304e1df428489cd0b3
HamzaUmer <mirzasahab123321@gmail.com>
2022-03-30 12:59:47 +0500

    My Portfolio

---
+++ src/data.js
@@ -0,0 +1,57 @@
+import Img1 from "./img/AvengersWebsite.png";
+import Img2 from "./img/UniversityWebsite.png";
+import Img3 from "./img/meetupApp.png";
+import Img4 from "./img/MERNapp.png";
+import Img5 from "./img/eBookStore.png";
+import Img6 from "./img/ScrambleWords.png";
+import Img7 from "./img/ARMart.png";
+import Img8 from "./img/MusicPlayer.png";
+import Img9 from "./img/NextJS.png";
+
+export const products = [
+    {
+      id: 1,
+      img: Img1,
+      link: "https://hamzaumer.github.io/Avengers-Website/",
+    },
+    {
+      id: 2,
+      img: Img2,
+      link: "https://hamzaumer.github.io/University-full-Responsive-Website-Practice/index.html",
+    },
+    {
+      id: 3,
+      img: Img3,
+      link: "https://meetupclone99.herokuapp.com",
+    },
+    {
+      id: 4,
+      img: Img4,
+      link: "https://mernbunnyapp.herokuapp.com/",
+    },
+    {
+      id: 5,
+      img: Img5,
+      link: "https://ebook-store-app.vercel.app/",
+    },
+    {
+      id: 6,
+      img: Img6,
+      link: "https://hamzaumer.github.io/Scrambled-Words-ES6/",
+    },
+    {
+      id: 7,
+      img: Img7,
+      link: "https://e-commerceappmern.herokuapp.com/",
+    },
+    {
+      id: 8,
+      img: Img8,
+      link: "https://hamzaumer.github.io/Music_Player_By_JS/",
+    },
+    {
+      id: 9,
+      img: Img9,
+      link: "https://github.com/HamzaUmer/NextJS-Restaurant-App",
+    },
+  ];
